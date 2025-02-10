87711af763e088cd60e92837cb15592eb05ea0f0
pablo <pblpawelmilczak@gmail.com>
2025-02-10 16:44:12 +0100

    Structure openai response by zod

--- src/services/generateSentence/generateSentenceAction.ts
+++ src/services/generateSentence/generateSentenceAction.ts
@@ -2,8 +2,16 @@
 
 import OpenAI from 'openai';
 import React from 'react';
+import { zodResponseFormat } from 'openai/helpers/zod';
+import { z } from 'zod';
 
 const openai = new OpenAI();
+
+const SentencesList = z.object({
+  word: z.string().describe('The word provided by user'),
+  usagesList: z.array(z.string()),
+  sentencesList: z.array(z.object({ usage: z.string(), sentence: z.string() })),
+});
 
 const oaiGenerateSentence = React.cache((sentence: string) =>
   openai.chat.completions.create({
@@ -11,37 +19,27 @@ const oaiGenerateSentence = React.cache((sentence: string) =>
     messages: [
       {
         role: 'developer',
+        // content:
+        //   'You generate 10 sentences for word provided by user. These sentences are used to learn vocabulary in mobile application.',
         content:
-          'You generate 10 sentences for word provided by user. These sentences are used to learn vocabulary in mobile application.',
+          'You generate data for mobile vocabulary learning app. User provides a word and you generate list of ALL possible meaning/usage of the word. For every meaning/usage, you generate a list of 10 sentences. I need exactly 10 sentences for each meaning/usage. The array length of sentencesList should be equal to the array length of usagesList * 10.',
       },
       {
         role: 'user',
         content: sentence,
       },
     ],
-    // response_format: {
-    //   // See /docs/guides/structured-outputs
-    //   type: 'json_schema',
-    //   json_schema: {
-    //     name: 'generated_sentences',
-    //     schema: {
-    //       type: 'object',
-    //       properties: {
-    //         email: {
-    //           description: 'The email address that appears in the input',
-    //           type: 'string',
-    //         },
-    //       },
-    //       additionalProperties: false,
-    //     },
-    //   },
-    // },
-    // store: true,
+    response_format: zodResponseFormat(SentencesList, 'sentence'),
+    store: true,
   })
 );
 
 export async function generateSentenceAction() {
   // Mutate data
   const res = await oaiGenerateSentence('apprehend');
-  return res.choices[0].message;
+
+  if (!res.choices[0].message.content)
+    throw new Error('No sentences for provided word');
+
+  return JSON.parse(res.choices[0].message.content);
 }
