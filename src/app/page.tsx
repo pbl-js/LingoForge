81bbd89c87cc9431a8981203a60c9f586a8a190a
pablo <pblpawelmilczak@gmail.com>
2025-02-11 13:38:02 +0100

    Add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { currentUser } from '@clerk/nextjs/server';
 export default async function Home() {
   const user = await currentUser();
 
-  console.log(user);
+  console.log(user?.id);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
